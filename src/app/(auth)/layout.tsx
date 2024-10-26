a19ea8710d976db371247871b859de8b3372c191
Hugo Lopes <hugo.rodrigue.lopes@gmail.com>
2024-10-26 12:03:07 +0100

    finish sign forms ui

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,25 +1,36 @@
-import { Button } from "@/components/ui/button";
+'use client';
+
+import {Button} from "@/components/ui/button";
 import Image from "next/image";
-import { ReactNode } from "react";
+import {ReactNode} from "react";
+import {usePathname} from "next/navigation";
+import Link from "next/link";
 
 interface AuthLayoutProps {
-  children: ReactNode;
+    children: ReactNode;
 }
 
-const AuthLayout = ({ children }: AuthLayoutProps) => {
-  return (
-    <main className="bg-neutral-100 min-h-screen">
-      <div className="mx-auto max-w-screen-2xl p-4">
-        <nav className="flex justify-between items-center">
-          <Image src="/logo.svg" height={56} width={152} alt="Logo" />
-          <Button variant="secondary">Sign Up</Button>
-        </nav>
-        <div className="flex flex-col items-center justify-center pt-4 md:pt-14">
-          {children}
-        </div>
-      </div>
-    </main>
-  );
+const AuthLayout = ({children}: AuthLayoutProps) => {
+    const pathname = usePathname();
+    const isSignUp = pathname === '/sign-up';
+
+    return (
+        <main className="bg-neutral-100 min-h-screen">
+            <div className="mx-auto max-w-screen-2xl p-4">
+                <nav className="flex justify-between items-center">
+                    <Image src="/logo.svg" height={56} width={152} alt="Logo"/>
+                    <Button asChild variant="secondary">
+                        <Link href={isSignUp ? '/sign-in' : '/sign-up'}>
+                            {isSignUp ? 'Login' : 'Sign Up'}
+                        </Link>
+                    </Button>
+                </nav>
+                <div className="flex flex-col items-center justify-center pt-4 md:pt-14">
+                    {children}
+                </div>
+            </div>
+        </main>
+    );
 };
 
 export default AuthLayout;
--- src/features/auth/components/sign-up-card.tsx
+++ src/features/auth/components/sign-up-card.tsx
@@ -5,8 +5,32 @@ import {DottedSeparator} from "@/components/dotted-separator";
 import {Input} from "@/components/ui/input";
 import {Button} from "@/components/ui/button";
 import Link from "next/link";
+import {z} from "zod";
+import {useForm} from "react-hook-form";
+import {zodResolver} from "@hookform/resolvers/zod";
+import {Form, FormControl, FormField, FormItem, FormMessage} from "@/components/ui/form";
+
+const formSchema = z.object({
+    name: z.string().trim().min(1, 'Required'),
+    email: z.string().email(),
+    password: z.string().trim().min(8, 'Minimum of 8 characters')
+});
 
 export const SignUpCard = () => {
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            name: '',
+            email: '',
+            password: ''
+        }
+    });
+
+    const onSubmit = (values: z.infer<typeof formSchema>) => {
+        console.log(values);
+    }
+
     return (
         <Card className="w-full h-full md:w-[487px] border-none shadow-none">
             <CardHeader className="flex items-center justify-center text-center p-7">
@@ -26,40 +50,60 @@ export const SignUpCard = () => {
                 <DottedSeparator/>
             </div>
             <CardContent className="p-7">
-                <form className="space-y-4">
-                    <Input
-                        required
-                        type="text"
-                        value={''}
-                        onChange={() => {
-                        }}
-                        placeholder="Enter your name"
-                        disabled={false}
-                    />
-                    <Input
-                        required
-                        type="email"
-                        value={''}
-                        onChange={() => {
-                        }}
-                        placeholder="Enter email"
-                        disabled={false}
-                    />
-                    <Input
-                        required
-                        type="password"
-                        value={''}
-                        onChange={() => {
-                        }}
-                        placeholder="Enter password"
-                        disabled={false}
-                        min={8}
-                        max={256}
-                    />
-                    <Button size="lg" className="w-full">
-                        Login
-                    </Button>
-                </form>
+                <Form {...form}>
+                    <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+                        <FormField
+                            name="name"
+                            control={form.control}
+                            render={({field}) => (
+                                <FormItem>
+                                    <FormControl>
+                                        <Input
+                                            type="name"
+                                            placeholder="Enter your name"
+                                            {...field}
+                                        />
+                                    </FormControl>
+                                    <FormMessage/>
+                                </FormItem>)}
+                        />
+                        <FormField
+                            name="email"
+                            control={form.control}
+                            render={({field}) => (
+                                <FormItem>
+                                    <FormControl>
+                                        <Input
+                                            type="email"
+                                            placeholder="Enter email"
+                                            {...field}
+                                        />
+                                    </FormControl>
+                                    <FormMessage/>
+                                </FormItem>)}
+                        />
+                        <FormField
+                            name="password"
+                            control={form.control}
+                            render={({field}) => (
+                                <FormItem>
+                                    <FormControl>
+                                        <Input
+                                            type="password"
+                                            placeholder="Enter password"
+                                            min={8}
+                                            max={256}
+                                            {...field}
+                                        />
+                                    </FormControl>
+                                    <FormMessage/>
+                                </FormItem>)}
+                        />
+                        <Button size="lg" className="w-full">
+                            Sign up
+                        </Button>
+                    </form>
+                </Form>
             </CardContent>
             <div className="px-7">
                 <DottedSeparator/>
@@ -73,6 +117,17 @@ export const SignUpCard = () => {
                     <FaGithub className="mr-2 size-5"/>
                     Login with Github
                 </Button>
+            </CardContent>
+            <div className="px-7">
+                <DottedSeparator/>
+            </div>
+            <CardContent className="p-7 flex items-center justify-center">
+                <p>
+                    Already have an account?
+                    <Link href="/sign-in">
+                        <span className="text-blue-700">&nbsp;Login</span>
+                    </Link>
+                </p>
             </CardContent>
         </Card>
     );
